8926ec2b14a87cbba8c415ace13cbf38e2f0ef42
m-iizuka <m.iizuka@ticnet.co.jp>
2025-11-18 16:34:34 +0900

    fix:Task.27で使い回せるように一部修正

--- trial_source/src/main/resources/static/js/attendance_check.js
+++ trial_source/src/main/resources/static/js/attendance_check.js
@@ -1,38 +1,8 @@
-/**
- * 過去日の未入力チェック
- */
-
-
-/*
-	// <tbody>内のすべての<tr>をリストで取得
-	let rows = document.querySelectorAll("tbody tr");
-
-	// 各行の内容確認_名前が「〇〇」で終わるもの
-	for (let row of rows) {
-		// 「研修日」
-		let date = row.querySelector("input[name$='.trainingDate']").value;
-		// 「出勤時間」
-		let start = row.querySelector("input[name$='.trainingStartTime']").value;
-		// 「退勤時間」
-		let end = row.querySelector("input[name$='.trainingEndTime']").value;
-		// Date型にフォーマット
-		let trainingDate = new Date(date);
-		// 今日の日付回収
-		let today = new Date();
-		today.setHours(0, 0, 0, 0);
-
-		// 過去日に未入力があるなら、エラーダイアログを出す
-		if (trainingDate < today && (start === "" || end === "")) {
-			alert("過去日の勤怠に未入力があります。");
-			e.preventDefault();
-			return false;
-		}
-	}
-	*/
-
+// 飯塚麻美子 - Task.27
 
 /**
  * 更新確認
+ * @author 飯塚麻美子 - Task.27
  */
 document.querySelector('input[name="complete"]').addEventListener('click', function(e) {
 	
